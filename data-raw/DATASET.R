25eb8d7342dc2259e892c465a3eaabe250939635
joseph osborne <joseph@osborne.uk.com>
2022-02-24 15:39:43 +0000

    renaming the source data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,27 +7,27 @@ library(stringr)
 source("R/intl.R")
 
 unzip("inst/extdata/qualification_round_2021.in.zip", exdir = "inst/extdata/qualification_round_2021")
-part_a <- "inst/extdata/qualification_round_2021/a.txt" %>%
+legacy_part_a <- "inst/extdata/qualification_round_2021/a.txt" %>%
         intl_input_parser()
 
-part_b <- "inst/extdata/qualification_round_2021/b.txt" %>%
+legacy_part_b <- "inst/extdata/qualification_round_2021/b.txt" %>%
         intl_input_parser()
 
-part_c <- "inst/extdata/qualification_round_2021/c.txt" %>%
+legacy_part_c <- "inst/extdata/qualification_round_2021/c.txt" %>%
         intl_input_parser()
 
-part_d <- "inst/extdata/qualification_round_2021/d.txt" %>%
+legacy_part_d <- "inst/extdata/qualification_round_2021/d.txt" %>%
         intl_input_parser()
 
-part_e <- "inst/extdata/qualification_round_2021/e.txt" %>%
+legacy_part_e <- "inst/extdata/qualification_round_2021/e.txt" %>%
         intl_input_parser()
 
-part_f <- "inst/extdata/qualification_round_2021/f.txt" %>%
+legacy_part_f <- "inst/extdata/qualification_round_2021/f.txt" %>%
         intl_input_parser()
 
-usethis::use_data(part_a, overwrite = TRUE)
-usethis::use_data(part_b, overwrite = TRUE)
-usethis::use_data(part_c, overwrite = TRUE)
-usethis::use_data(part_d, overwrite = TRUE)
-usethis::use_data(part_e, overwrite = TRUE)
-usethis::use_data(part_f, overwrite = TRUE)
+usethis::use_data(legacy_part_a, overwrite = TRUE)
+usethis::use_data(legacy_part_b, overwrite = TRUE)
+usethis::use_data(legacy_part_c, overwrite = TRUE)
+usethis::use_data(legacy_part_d, overwrite = TRUE)
+usethis::use_data(legacy_part_e, overwrite = TRUE)
+usethis::use_data(legacy_part_f, overwrite = TRUE)
